8bfa1f5c9d09a28204654f1c8da84fc22c795c54
meryldakin <meryl.k.dakin@gmail.com>
2017-08-14 15:28:30 -0400

    removed new edit and delete paths

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -9,35 +9,4 @@ class DrinksController < ApplicationController
     @drink = Drink.find(params[:id])
   end
 
-  def new
-    @drink = Drink.new
-  end
-
-  def create
-    @drink = Drink.create(drinks_params)
-    redirect_to drinks_path
-  end
-
-  def edit
-    @drink = Drink.find(params[:id])
-  end
-
-  def update
-    @drink = Drink.find(params[:id])
-    @drink.update(drinks_params)
-    redirect_to drink_path
-  end
-
-  def destroy
-    @drink = Drink.find(params[:id])
-    @drink.destroy
-    redirect_to drinks_path
-  end
-
-  private
-
-  def drinks_params
-    params.require(:drink).permit(:name, :price)
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  # resources :drinks, only: [:index, :show, :new, :create]
+  # resources :drinks, only: [:index, :show]
   get '/drinks', to: 'drinks#index', as: 'drinks'
-  post '/drinks', to: 'drinks#create'
-  get '/drinks/new', to: 'drinks#new', as: 'new_drink'
   get '/drinks/:id', to: 'drinks#show', as: 'drink'
-  get '/drinks/:id/edit', to: 'drinks#edit', as: 'edit_drink'
-  patch '/drinks/:id', to: 'drinks#update'
-  delete '/drinks/:id', to: 'drinks#destroy'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
